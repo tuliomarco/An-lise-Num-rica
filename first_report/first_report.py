a22c075ea2214c064c1ad1cc63ceadcf2e596f1e
Marco Túlio Macêdo <marcomacedo018@gmail.com>
2024-09-25 16:26:53 -0300

    adding implementation of false_position method

--- first_report/first_report.py
+++ first_report/first_report.py
@@ -1,10 +1,12 @@
 import root_finding
 
-print("Qual método você deseja testar?\n1. Bissecção")
+print("Qual método você deseja testar?\n1. Bissecção\n2.Posição falsa")
 method = int(input())
 
 r = root_finding.Roots()
 if(method == 1):
     r.bisection()
+elif(method == 2):
+    r.false_position()
 else:
     print("Método ainda não implementado ou inexistente")
--- first_report/root_finding.py
+++ first_report/root_finding.py
@@ -14,6 +14,10 @@ class Roots:
             inputs = inputs[-1].split("\n")
             self.func = inputs[0]
 
+            if self.funcao(self.a) * self.funcao(self.b) > 0:
+                print("O intervalo não contém raiz")
+                return
+            
         except Exception as e:
             print(f"Erro ao ler arquivo: {e}")
             sys.exit()
@@ -22,31 +26,52 @@ class Roots:
         return eval(self.func)
 
     def bisection(self):
-        i = 0
+        k = 0
         a = self.a
         b = self.b
         t = self.t
 
-        if self.funcao(a) * self.funcao(b) > 0:
-            print("O intervalo não contém raiz")
-            return
-
         with open("roots_output.txt", "w") as arq:
-            arq.write(" i |    a    |    b    |   f(a)   |   f(b)  |   b-a   | (b-a)/x\n")
+            arq.write(" k |    a    |    b    |   f(a)   |   f(b)  |   b-a   | (b-a)/x\n")
             
             while True:
                 x = (a + b) / 2
                 aproximacao = (b - a) / 2
                 erro_relativo = (b - a) / max(abs(x), 1e-10)
 
-                i += 1
-                arq.write(f"{i:2} | {a:.5f} | {b:.5f} | {self.funcao(a):.5f} | {self.funcao(b):.5f} | {b-a:.5f} | {erro_relativo:.5f}\n")
+                k += 1
+                arq.write(f"{k:2} | {a:.5f} | {b:.5f} | {self.funcao(a):.5f} | {self.funcao(b):.5f} | {b-a:.5f} | {erro_relativo:.5f}\n")
 
                 if abs(self.funcao(x)) < t or aproximacao < t:
-                    print(f"Raiz encontrada: {x:.5f} após {i} iterações.")
+                    print(f"Raiz encontrada: {x:.5f} após {k} iterações.")
                     return
 
                 if self.funcao(a) * self.funcao(x) < 0:
                     b = x
                 else:
                     a = x
+    
+    def false_position(self):
+            k = 0
+            a = self.a
+            b = self.b
+            tol = self.t
+
+            with open("roots_output_fp.txt", "w") as arq:
+                arq.write(" k |    a    |    b    |   f(a)   |   f(b)  |   b-a   |    c    |   f(c)\n")
+
+                while True:
+                    c = (a * self.funcao(b) - b * self.funcao(a)) / (self.funcao(b) - self.funcao(a))
+                    e = abs(b - a) / max(abs(c), 1e-10)
+
+                    k += 1
+                    arq.write(f"{k:2} | {a:.5f} | {b:.5f} | {self.funcao(a):.5f} | {self.funcao(b):.5f} | {b-a:.5f} | {c:.5f} | {self.funcao(c):.5f}\n")
+
+                    if abs(self.funcao(c)) < tol or e < tol:
+                        print(f"Raiz encontrada: {c:.5f} após {k} iterações.")
+                        return
+
+                    if self.funcao(a) * self.funcao(c) < 0:
+                        b = c
+                    else:
+                        a = c
