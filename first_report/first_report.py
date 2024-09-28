0d7a7384677c7029effc1870712b2adacc7c975b
Marco Túlio Macêdo <marcomacedo018@gmail.com>
2024-09-28 19:20:53 -0300

    finishing root finding methods

--- first_report/first_report.py
+++ first_report/first_report.py
@@ -1,6 +1,7 @@
 import root_finding
 
-print("Qual método você deseja testar?\n1. Bissecção\n2.Posição falsa")
+print("Qual método você deseja testar?"
+      "\n1. Bissecção\n2. Posição falsa\n3. Newton-Raphson\n4. Secante")
 method = int(input())
 
 r = root_finding.Roots()
@@ -8,5 +9,9 @@ if(method == 1):
     r.bisection()
 elif(method == 2):
     r.false_position()
+elif(method == 3):
+    r.newton_raphson()
+elif(method == 4):
+    r.secant()
 else:
     print("Método ainda não implementado ou inexistente")
--- first_report/root_finding.py
+++ first_report/root_finding.py
@@ -1,4 +1,9 @@
 import sys
+from math import *
+from sympy import *
+
+def function(x, f):
+    return eval(f)
 
 class Roots:
     def __init__(self):
@@ -7,49 +12,57 @@ class Roots:
             with open(file_name, "r") as file:
                 inputs = file.read().split(";")
             
+            self.len_inputs = len(inputs)
             self.a = float(inputs[0])
-            self.b = float(inputs[1])
-            self.t = eval(inputs[2])
+            self.b = float(inputs[1]) if self.len_inputs == 4 else None
+            self.t = eval(inputs[2 if self.len_inputs == 4 else 1])
             
             inputs = inputs[-1].split("\n")
-            self.func = inputs[0]
-
-            if self.funcao(self.a) * self.funcao(self.b) > 0:
-                print("O intervalo não contém raiz")
-                return
+            self.func = inputs[0].strip()
             
         except Exception as e:
             print(f"Erro ao ler arquivo: {e}")
             sys.exit()
+    
+    def derivative(self, a):
+        x = Symbol("x")
+        symbolic_func = eval(self.func)
+        symbolic_derivative = symbolic_func.diff(x)
+        return symbolic_derivative.subs(x, a)
 
-    def funcao(self, x):
-        return eval(self.func)
 
     def bisection(self):
         k = 0
         a = self.a
         b = self.b
-        t = self.t
+        tol = self.t
 
-        with open("roots_output.txt", "w") as arq:
-            arq.write(" k |    a    |    b    |   f(a)   |   f(b)  |   b-a   | (b-a)/x\n")
+        if self.len_inputs < 4: 
+            print("\nFormato esperado: <a>;<b>;<tol>;<function>\n")
+            return
+        elif function(a, self.func) * function(b, self.func) > 0:
+            print("O intervalo não contém raiz")
+            return
+
+        with open("roots_output.txt", "w") as file:
+            file.write(" k |     a     |     b     |   f(a)    |    f(b)   |    b-a    |  (b-a)/a  |     c     |   f(c)    \n")
             
             while True:
-                x = (a + b) / 2
-                aproximacao = (b - a) / 2
-                erro_relativo = (b - a) / max(abs(x), 1e-10)
+                c = (a + b) / 2
+                erro_relativo = abs(b - a) / (abs(a) if a != 0 else 1)
 
                 k += 1
-                arq.write(f"{k:2} | {a:.5f} | {b:.5f} | {self.funcao(a):.5f} | {self.funcao(b):.5f} | {b-a:.5f} | {erro_relativo:.5f}\n")
+                file.write(f"{k:2} | {a:9.5f} | {b:9.5f} | {function(a, self.func):9.5f} | {function(b, self.func):9.5f} | {b-a:9.5f} | {erro_relativo:9.5f} | {c:9.5f} | {function(c, self.func):9.5f}\n")
 
-                if abs(self.funcao(x)) < t or aproximacao < t:
-                    print(f"Raiz encontrada: {x:.5f} após {k} iterações.")
+                if abs(function(c, self.func)) < tol or abs(b-a) < tol:
+                    file.write(f"\nResultado {c:.5f} obtido pelo método da bissecção.")
                     return
 
-                if self.funcao(a) * self.funcao(x) < 0:
-                    b = x
+                if function(a, self.func) * function(c, self.func) < 0:
+                    b = c
                 else:
-                    a = x
+                    a = c
+
     
     def false_position(self):
             k = 0
@@ -57,21 +70,90 @@ class Roots:
             b = self.b
             tol = self.t
 
-            with open("roots_output_fp.txt", "w") as arq:
-                arq.write(" k |    a    |    b    |   f(a)   |   f(b)  |   b-a   |    c    |   f(c)\n")
+            if(self.len_inputs < 4): 
+                print("\nFormato esperado: <a>;<b>;<tol>;<function>\n")
+                return
+            if function(a, self.func) * function(b, self.func) > 0:
+                print("O intervalo não contém raiz")
+                return
+
+            with open("roots_output.txt", "w") as file:
+                file.write(" k |     a     |     b     |    f(a)   |    f(b)   |    b-a    |     c     |   f(c)\n")
 
                 while True:
-                    c = (a * self.funcao(b) - b * self.funcao(a)) / (self.funcao(b) - self.funcao(a))
-                    e = abs(b - a) / max(abs(c), 1e-10)
+                    c = (a * function(b, self.func) - b * function(a, self.func)) / (function(b, self.func) - function(a, self.func))
+                    relative_error = abs(b - a) / max(abs(c), 1e-10)
 
                     k += 1
-                    arq.write(f"{k:2} | {a:.5f} | {b:.5f} | {self.funcao(a):.5f} | {self.funcao(b):.5f} | {b-a:.5f} | {c:.5f} | {self.funcao(c):.5f}\n")
+                    file.write(f"{k:2} | {a:9.5f} | {b:9.5f} | {function(a, self.func):9.5f} | {function(b, self.func):9.5f} | {b-a:9.5f} | {c:9.5f} | {function(c, self.func):9.5f}\n")
 
-                    if abs(self.funcao(c)) < tol or e < tol:
-                        print(f"Raiz encontrada: {c:.5f} após {k} iterações.")
+                    if abs(function(c, self.func)) < tol or relative_error < tol:
+                        file.write(f"\nResultado {c:.5f} obtido pelo método da posição falsa.")
                         return
 
-                    if self.funcao(a) * self.funcao(c) < 0:
+                    if function(a, self.func) * function(c, self.func) < 0:
                         b = c
                     else:
                         a = c
+
+    def newton_raphson(self):
+        i = 0
+        a = self.a
+        tol = self.t
+
+        if(self.len_inputs < 3): 
+            print("\nFormato esperado: <a>;<tol>;<function>\n")
+            return
+                
+        with open("roots_output.txt", "w") as file:
+            file.write(" k |    a    |   f(a)   |  f'(a)  |  Erro relativo \n")
+            
+            while True:
+                i += 1
+                fa = function(a, self.func)
+                f_deriv = self.derivative(a)
+                
+                if f_deriv == 0 or f_deriv < 10**-10:
+                    print("A derivada é zero! O método de Newton-Raphson falhou.")
+                    file.write(f"{i:2} | {a:7.5f} | {fa:8.5f} | {f_deriv:7.5f} |  Derivada zero, método falhou\n")
+                    return
+                
+                delta = fa / f_deriv
+                a -= delta
+                relative_error = abs(delta) / max(abs(a), 1e-10)
+
+                file.write(f"{i:2} | {a:7.5f} | {fa:8.5f} | {f_deriv:7.5f} | {relative_error:8.5f}\n")
+
+                if abs(fa) < tol or relative_error < tol:
+                    file.write(f"\nResultado {a:.5f} obtido pelo método de Newton-Raphson.")
+                    return
+    
+    def secant(self):
+        k = 0
+        a = self.a
+        b = self.b
+        tol = self.t
+
+        if(self.len_inputs < 4): 
+            print("\nFormato esperado: <a>;<b>;<tol>;<function>\n")
+            return
+        
+        with open("roots_output.txt", "w") as file:
+            file.write(" k |    a    |    b    |    x    |   f(a)   |   f(b)   |   f(x)   |  Erro Relativo \n")
+
+            while True:
+                f_a = function(a, self.func)
+                f_b = function(b, self.func)
+                c = b - f_b * (a - b) / (f_a - f_b)
+                f_c = function(c, self.func)
+
+                relative_error = abs((c - b) / max(abs(c), 1e-10))
+
+                k += 1
+                file.write(f"{k:2} | {a:7.5f} | {b:7.5f} | {c:7.5f} | {f_a:8.5f} | {f_b:8.5f} | {f_c:8.5f} | {relative_error:8.5f}\n")
+
+                if abs(f_c) < tol or relative_error < tol:
+                    file.write(f"\nResultado {c:.5f} obtido pelo método da secante.")
+                    return
+
+                a, b = b, c
